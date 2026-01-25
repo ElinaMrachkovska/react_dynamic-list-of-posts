d9a4fe867a8f3cec7ad828cfab8aadbaaebf09ed
Elina <elina@MacBook-Pro-Elina.local>
2026-01-25 13:39:44 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -7,35 +7,76 @@ import './App.scss';
 import { PostsList } from './components/PostsList';
 import { PostDetails } from './components/PostDetails';
 import { UserSelector } from './components/UserSelector';
-import { Loader } from './components/Loader';
+import { Loader } from './components/Loader/Loader';
+import { useState } from 'react';
+import { User } from './types/User';
+import { Post } from './types/Post';
+import { client } from './utils/fetchClient';
 
-export const App = () => (
-  <main className="section">
-    <div className="container">
-      <div className="tile is-ancestor">
+
+export const App = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  const [selectedPost, setSelectedPost] = useState<Post | null>(null)
+const [isPostLoading, setIsPostLoading] = useState(false);
+const [hasPostLoadingError, setHasPostLoadingError] = useState(false);
+
+const handleUserSelect = (user: User) => {
+  setPosts([]);
+  setSelectedUser(user);
+  setIsPostLoading(true);
+  setSelectedPost(null);
+  setHasPostLoadingError(false)
+
+  client 
+    .get<Post[]>(`/posts?userId=${user.id}`)
+    .then(setPosts)
+    .catch(() => {
+      setHasPostLoadingError(true);
+    })
+    .finally(() => {
+      setIsPostLoading(false);
+    });
+  }
+const canShowPostDetails = selectedUser && !isPostLoading && !hasPostLoadingError;
+
+
+
+  return (
+    <main className="section">
+      <div className="container">
+        <div className="tile is-ancestor">
         <div className="tile is-parent">
           <div className="tile is-child box is-success">
-            <div className="block">
-              <UserSelector />
+            <div
+              className="block">
+              <UserSelector user={selectedUser} onSelect={handleUserSelect} />
             </div>
 
             <div className="block" data-cy="MainContent">
-              <p data-cy="NoSelectedUser">No user selected</p>
+              {!selectedUser && (
+                <p data-cy="NoSelectedUser">No user selected</p>
+              )}
+              {selectedUser && isPostLoading &&  <Loader />}
+              {selectedUser && !isPostLoading && hasPostLoadingError && (
+                <div className="notification is-danger">
+                  Something went wrong!
+                </div>
+              )}
+              {canShowPostDetails && posts.length === 0 && (
+                <div className="notification is-warning">
+                  No posts yet
+                </div>
+              )}
 
-              <Loader />
+{canShowPostDetails && posts.length > 0 && (
+  <PostsList
+    posts={posts}
+    selectedPostId={selectedPost?.id ?? null}
+    onSelectPost={setSelectedPost}
+  />
+)}
 
-              <div
-                className="notification is-danger"
-                data-cy="PostsLoadingError"
-              >
-                Something went wrong!
-              </div>
-
-              <div className="notification is-warning" data-cy="NoPostsYet">
-                No posts yet
-              </div>
-
-              <PostsList />
             </div>
           </div>
         </div>
@@ -47,14 +88,15 @@ export const App = () => (
             'is-parent',
             'is-8-desktop',
             'Sidebar',
-            'Sidebar--open',
+            {'Sidebar--open': !!selectedPost}
           )}
         >
           <div className="tile is-child box is-success ">
-            <PostDetails />
+            <PostDetails post={selectedPost} />
           </div>
         </div>
       </div>
     </div>
   </main>
 );
+}
